eec44c4a91c2203921dcf467525ef299872d3b98
Garo Garo <72091112+Garo01@users.noreply.github.com>
2020-10-14 18:01:01 +0300

    Create NumberCounter.java

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,28 @@
+package exercise3; 
+import java.util.Scanner ;
+public class NumberCounter {
+public static void main (String[]args){
+  Scanner input = new Scanner(System.in); 
+  int negative = 0;
+  int positive = 0;
+  int sum = 0; 
+  int number = input.nextInt();
+  while (number !=0) {
+    if (number>0){positive++;}
+    else {negative++;} 
+    sum += number;
+    number= input.nextInt();
+    //sum= sum+number//
+  }
+  float average = sum/(positive + negative);
+
+ System.out.println("positive");
+ System.out.println(negative);
+ System.out.println (sum);
+ System.out.println (average);
+
+}
+    
+
+
+  }
